632a8caf87169f7b2a2fd74bd2bd076b138c1abe
thijsBoet <m.boet@chello.nl>
2022-05-03 01:06:21 +0200

    restarting from scratch

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      hello world
+    </div>
+  )
+}
+
+export default App
