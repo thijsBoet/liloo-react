898c61c6fa8a4f4897e52016bfb32420d0c3ea28
thijsBoet <m.boet@chello.nl>
2022-05-03 18:28:45 +0200

    setting up basic layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
-import { useState } from 'react'
+import Navbar from './components/Navbar'
+
+
+
+import './app.css'
 
 const App = () => {
-  const [count, setCount] = useState(0)
-
   return (
-    <div>
-      hello world
+    <div className="app bg-gray-100 h-screen">
+      <div className='container bg-white'>
+        <Navbar />
+      </div>
     </div>
   )
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Button from '../ui/Button'
+
+const Navbar = () => {
+  return (
+    <nav className='flex flex-row text-center mt-4'>
+
+    </nav>
+  )
+}
+
+export default Navbar
